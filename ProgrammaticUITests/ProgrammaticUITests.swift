8279ed7f8a4b1b20cf5f81cabd8f47180fba2809
Subham Kaumar <patelsk7481@gmail.com>
2025-04-23 10:05:54 +0530

    Initial Commit

---
+++ ProgrammaticUITests/ProgrammaticUITests.swift
@@ -0,0 +1,17 @@
+//
+//  ProgrammaticUITests.swift
+//  ProgrammaticUITests
+//
+//  Created by Subham Patel on 23/04/25.
+//
+
+import Testing
+@testable import ProgrammaticUI
+
+struct ProgrammaticUITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
