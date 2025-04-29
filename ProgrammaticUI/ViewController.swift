32d21b518f2eaf5bbc8113305098457ff9eb4687
Subham Kaumar <patelsk7481@gmail.com>
2025-04-29 23:26:09 +0530

    Initial Commit.

--- ProgrammaticUI/ViewController.swift
+++ ProgrammaticUI/ViewController.swift
@@ -8,12 +8,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    let nextButton = UIButton()
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
+        let myCustomView = MyCustomView()
+        myCustomView.frame = CGRect(x: 0, y: 100, width: 300, height: 200)
+        myCustomView.backgroundColor = UIColor.gray
+        myCustomView.layer.cornerRadius = 10
+        view.addSubview(myCustomView)
+        myCustomView.useComponents()
     }
-
-
 }
-
+class MyCustomView: UIView{
+    @objc func useComponents(){
+        let myLabel = UILabel()
+        myLabel.text = "Using Components of UIKit for the learning purpose."
+        myLabel.textColor = .white
+        myLabel.backgroundColor = .green
+        myLabel.textAlignment = .center
+        myLabel.frame = CGRect(x: 50, y: 100, width: 200, height: 40)
+        self.addSubview(myLabel)
+    }
+}
