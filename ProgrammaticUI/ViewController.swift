182d870088c174c0447112407817a132d616158c
Subham Kaumar <patelsk7481@gmail.com>
2025-05-02 21:05:01 +0530

    Learned new things.

--- ProgrammaticUI/ViewController.swift
+++ ProgrammaticUI/ViewController.swift
@@ -7,27 +7,101 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-    let nextButton = UIButton()
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = data[indexPath.row]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath){
+        print("Selected: \(data[indexPath.row])")
+        switch indexPath.row{
+        case 0:
+            navigationController?.pushViewController(AppleViewController(), animated: true)
+            
+        case 1:
+            navigationController?.pushViewController(BananaViewController(), animated: true)
+            
+        case 2:
+            navigationController?.pushViewController(MangoViewController(), animated: true)
+            
+        case 3:
+            navigationController?.pushViewController(OrangeViewController(), animated: true)
+            
+        case 4:
+            navigationController?.pushViewController(PineappleViewController(), animated: true)
+            
+        default:
+            break
+        }
+    }
+    
+    let tableView = UITableView()
+    let data = ["Apple", "Banana", "Mango", "Orange", "Pineapple"]
     override func viewDidLoad() {
         super.viewDidLoad()
-       
-        let myCustomView = MyCustomView()
-        myCustomView.frame = CGRect(x: 0, y: 100, width: 300, height: 200)
-        myCustomView.backgroundColor = UIColor.gray
-        myCustomView.layer.cornerRadius = 10
-        view.addSubview(myCustomView)
-        myCustomView.useComponents()
+        view.backgroundColor = .white
+        title = "fruits"
+        
+        tableView.frame = view.bounds
+        tableView.delegate = self
+        tableView.dataSource = self
+        view.addSubview(tableView)
+        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
     }
 }
-class MyCustomView: UIView{
-    @objc func useComponents(){
-        let myLabel = UILabel()
-        myLabel.text = "Using Components of UIKit for the learning purpose."
-        myLabel.textColor = .white
-        myLabel.backgroundColor = .green
-        myLabel.textAlignment = .center
-        myLabel.frame = CGRect(x: 50, y: 100, width: 200, height: 40)
-        self.addSubview(myLabel)
+
+class AppleViewController: UIViewController{
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Apple"
+        view.backgroundColor = .red
+    }
+}
+class BananaViewController: UIViewController{
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Banana"
+        view.backgroundColor = .yellow
+    }
+}
+class MangoViewController: UIViewController{
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Mango"
+        view.backgroundColor = .green
+    }
+}
+class OrangeViewController: UIViewController{
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Orange"
+        view.backgroundColor = .orange
+    }
+}
+class PineappleViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Pineapple"
+        view.backgroundColor = .green
+
+        let descriptionLabel = UILabel()
+        descriptionLabel.text = "Pineapple is good for health."
+        descriptionLabel.textColor = .white
+        descriptionLabel.font = UIFont.systemFont(ofSize: 20)
+        descriptionLabel.textAlignment = .center
+        descriptionLabel.numberOfLines = 0
+
+        // Set frame or use Auto Layout
+        descriptionLabel.frame = CGRect(x: 20, y: 200, width: view.frame.width - 40, height: 100)
+
+        // Add to the view
+        view.addSubview(descriptionLabel)
     }
 }
