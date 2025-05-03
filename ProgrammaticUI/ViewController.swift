47fe5f131ad2b37efac4ad0cad3f1502148e7dc9
Subham Kaumar <patelsk7481@gmail.com>
2025-05-03 17:34:20 +0530

    Learned UICollectionView today.

--- ProgrammaticUI/ViewController.swift
+++ ProgrammaticUI/ViewController.swift
@@ -7,101 +7,57 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return data.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        cell.textLabel?.text = data[indexPath.row]
-        return cell
-    }
-    
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath){
-        print("Selected: \(data[indexPath.row])")
-        switch indexPath.row{
-        case 0:
-            navigationController?.pushViewController(AppleViewController(), animated: true)
-            
-        case 1:
-            navigationController?.pushViewController(BananaViewController(), animated: true)
-            
-        case 2:
-            navigationController?.pushViewController(MangoViewController(), animated: true)
-            
-        case 3:
-            navigationController?.pushViewController(OrangeViewController(), animated: true)
-            
-        case 4:
-            navigationController?.pushViewController(PineappleViewController(), animated: true)
-            
-        default:
-            break
-        }
-    }
-    
-    let tableView = UITableView()
-    let data = ["Apple", "Banana", "Mango", "Orange", "Pineapple"]
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource{
+    var collectionView: UICollectionView!
+    let items = Array(1...20)
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
-        title = "fruits"
+       
+        //creating layout
+        let layout = UICollectionViewFlowLayout()
+        let padding: CGFloat = 10
+        let itemWidth = (view.frame.width - 3 * padding) / 2
+        layout.itemSize = CGSize(width: itemWidth, height: itemWidth)
+        layout.sectionInset = UIEdgeInsets(top: padding, left: padding, bottom: padding, right: padding)
+        layout.minimumInteritemSpacing = padding
+        layout.minimumLineSpacing = padding
         
-        tableView.frame = view.bounds
-        tableView.delegate = self
-        tableView.dataSource = self
-        view.addSubview(tableView)
+        //Create CollectionView
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: layout)
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.register(MyCell.self, forCellWithReuseIdentifier: "Cell")
+        collectionView.backgroundColor = .white
         
-        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        view.addSubview(collectionView)
+    }
+    //DataSource Methods
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as! MyCell
+        cell.label.text = "\(items[indexPath.item])"
+        return cell
     }
 }
 
-class AppleViewController: UIViewController{
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        title = "Apple"
-        view.backgroundColor = .red
+class MyCell: UICollectionViewCell{
+    let label = UILabel()
+    
+    override init(frame: CGRect){
+        super.init(frame: frame)
+        contentView.backgroundColor = .systemGreen
+        label.textAlignment = .center
+        label.frame = contentView.bounds
+        contentView.addSubview(label)
+        contentView.layer.cornerRadius = 8
+        contentView.clipsToBounds = true
     }
-}
-class BananaViewController: UIViewController{
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        title = "Banana"
-        view.backgroundColor = .yellow
-    }
-}
-class MangoViewController: UIViewController{
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        title = "Mango"
-        view.backgroundColor = .green
-    }
-}
-class OrangeViewController: UIViewController{
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        title = "Orange"
-        view.backgroundColor = .orange
-    }
-}
-class PineappleViewController: UIViewController {
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        title = "Pineapple"
-        view.backgroundColor = .green
-
-        let descriptionLabel = UILabel()
-        descriptionLabel.text = "Pineapple is good for health."
-        descriptionLabel.textColor = .white
-        descriptionLabel.font = UIFont.systemFont(ofSize: 20)
-        descriptionLabel.textAlignment = .center
-        descriptionLabel.numberOfLines = 0
-
-        // Set frame or use Auto Layout
-        descriptionLabel.frame = CGRect(x: 20, y: 200, width: view.frame.width - 40, height: 100)
-
-        // Add to the view
-        view.addSubview(descriptionLabel)
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder: ) has not been implemented.")
     }
 }
