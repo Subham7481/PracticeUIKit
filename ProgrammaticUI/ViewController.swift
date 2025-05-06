4aa3d3b20f1d5eb331dd0208d6e6e708f55a0772
Subham Kaumar <patelsk7481@gmail.com>
2025-05-06 16:20:55 +0530

    Called an api and fetched data for the learning purpose using UIKit.

--- ProgrammaticUI/ViewController.swift
+++ ProgrammaticUI/ViewController.swift
@@ -7,57 +7,53 @@
 
 import UIKit
 
-class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource{
-    var collectionView: UICollectionView!
-    let items = Array(1...20)
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        view.backgroundColor = .white
-       
-        //creating layout
-        let layout = UICollectionViewFlowLayout()
-        let padding: CGFloat = 10
-        let itemWidth = (view.frame.width - 3 * padding) / 2
-        layout.itemSize = CGSize(width: itemWidth, height: itemWidth)
-        layout.sectionInset = UIEdgeInsets(top: padding, left: padding, bottom: padding, right: padding)
-        layout.minimumInteritemSpacing = padding
-        layout.minimumLineSpacing = padding
-        
-        //Create CollectionView
-        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: layout)
-        collectionView.delegate = self
-        collectionView.dataSource = self
-        collectionView.register(MyCell.self, forCellWithReuseIdentifier: "Cell")
-        collectionView.backgroundColor = .white
-        
-        view.addSubview(collectionView)
-    }
-    //DataSource Methods
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return items.count
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as! MyCell
-        cell.label.text = "\(items[indexPath.item])"
-        return cell
-    }
+struct Post: Codable {
+    let id: Int
+    let title: String
 }
 
-class MyCell: UICollectionViewCell{
-    let label = UILabel()
+class ViewController: UIViewController, UITableViewDataSource {
     
-    override init(frame: CGRect){
-        super.init(frame: frame)
-        contentView.backgroundColor = .systemGreen
-        label.textAlignment = .center
-        label.frame = contentView.bounds
-        contentView.addSubview(label)
-        contentView.layer.cornerRadius = 8
-        contentView.clipsToBounds = true
+    var fruits: [Post] = []
+    let tableView = UITableView()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.addSubview(tableView)
+        tableView.frame = view.bounds
+        tableView.dataSource = self
+        
+        fetchPost()
     }
     
-    required init?(coder: NSCoder) {
-        fatalError("init(coder: ) has not been implemented.")
+    func fetchPost() {
+        guard let url = URL(string: "https://jsonplaceholder.typicode.com/posts") else { return }
+        
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            if let data = data {
+                do {
+                    let decoded = try JSONDecoder().decode([Post].self, from: data)
+                    DispatchQueue.main.async {
+                        self.fruits = decoded
+                        self.tableView.reloadData()
+                    }
+                } catch {
+                    print("Decoding error: \(error)")
+                }
+            }
+        }.resume()
+    }
+    
+    // MARK: - TableView DataSource Methods
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return fruits.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = fruits[indexPath.row].title
+        return cell
     }
 }
